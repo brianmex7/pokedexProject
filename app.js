ec81861d31a8e69628e2c233607ddfd43e313bf9
brianmex7 <brianmexico77@gmail.com>
2024-03-19 19:18:35 -0600

    quick api practice

---
+++ app.js
@@ -0,0 +1,36 @@
+const pokedex = document.getElementById("pokedex");
+
+const fetchPokemon = () => {
+  promises = [];
+  for (let i = 1; i <= 250; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((data) => ({
+      name: data.name,
+      id: data.id,
+      image: data.sprites["front_default"],
+      type: data.types.map((type) => type.type.name).join(", "),
+    }));
+    displayPokemon(pokemon);
+  });
+};
+
+const displayPokemon = (pokemon) => {
+  const pokemonHTMLString = pokemon
+    .map(
+      (poke) =>
+        `
+  <li class="card">
+    <img class="card-img" src="${poke.image}"/>
+    <h2 class="card-title">${poke.id}. ${poke.name}</h2>
+    <p class="card-subtitle">Type: ${poke.type}</p>
+  </li>
+  `
+    )
+    .join("");
+  pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
